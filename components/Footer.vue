7fdb5d28e72633bd28660169da65bef16e587fa7
Navruz <navruz.halimov.3999@gmail.com>
2020-08-21 16:37:04 +0300

    start to

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,7 +7,7 @@
                         <div class="footer__item">
                             <div class="footer__logo">
                                 <nuxt-link to="" class="footer__logo-link">
-                                    <img src="../assets/images/main-logo.svg" alt="logo web page">
+                                    <img src="../assets/images/Ресурс 15.svg" alt="logo web page">
                                     <span class="legaltech">LegalTech</span>
                                 </nuxt-link>
                                 <div class="footer__motto">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
