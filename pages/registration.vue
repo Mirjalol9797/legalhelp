989d29bf4f22bc9c07ed52cc066e9c3970b76b93
Navruz <navruz.halimov.3999@gmail.com>
2020-08-28 05:57:56 -0700

    favicon fixed

--- pages/registration.vue
+++ pages/registration.vue
@@ -5,9 +5,37 @@
 			<div class="registration">
 				<div class="registration__inner">
 					<form action="" method="">
-						
+						<div class="name">
 						 <label for="name">Ism</label>
 						 <input type="text" id="name">
+						</div>
+						<div class="surname">
+							<label for="surname">Familya</label>
+							<input type="text" id="surname">
+						</div>
+						<div class="city">
+							<label for="city">Shahar</label>
+							<input type="text" id="city">
+						</div>
+						<div class="phone__number">
+							<label for="phone">Tel raqam</label>
+							<input type="tel" id="phone">
+						</div>
+						<div class="language">
+							<label for="language">Til</label>
+							<select name="" id="language">
+								<option value="o'zbek">O’zbek</option>
+								<option value="Rus">Rus</option>
+								<option value="Ingliz">Ingliz</option>
+								<option value="o'zbek">O’zbek</option>
+							</select>
+						</div>
+						<div class="category">
+							<label for="category">Kategoriya</label>
+							<select name="" id="category" value="Tanlash">
+								<option value=""></option>
+							</select>
+						</div>
 					</form>
 				</div>
 			</div>
