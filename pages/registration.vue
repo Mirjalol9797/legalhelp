c1aac244500a5ed0586e57d934ca948de1297abd
Navruz <navruz.halimov.3999@gmail.com>
2020-08-28 05:32:34 -0700

    registration added

---
+++ pages/registration.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<div class="registration__wrapper">
+			<b-container>
+			<div class="registration">
+				<div class="registration__inner">
+					<form action="" method="">
+						
+						 <label for="name">Ism</label>
+						 <input type="text" id="name">
+					</form>
+				</div>
+			</div>
+			</b-container>
+		</div>
+	</div>
+</template>
