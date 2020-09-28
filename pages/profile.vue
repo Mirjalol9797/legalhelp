3f97aef38a829ffc2a0d564b75aeb1cf634000d5
Navruz <navruz.halimov.3999@gmail.com>
2020-09-27 23:49:26 -0700

    changes

--- pages/profile.vue
+++ pages/profile.vue
@@ -4,12 +4,7 @@
       <b-container>
         <div class="profile-inner">
           <div class="owner-pic">
-            <img
-              src="../assets/images/lawyer-card/member2.jpg"
-              height="170"
-              width="264"
-              alt=""
-            />
+            <img src="../assets/images/lawyer-card/member2.jpg" height="170" width="264" alt />
           </div>
           <div class="profile-info">
             <span class="profile-info-name">John Smith</span>
@@ -24,104 +19,130 @@
               <b-tab active>
                 <template v-slot:title>
                   <span class="user__profile-card-icon">
-                    <font-awesome-icon :icon="['fas', 'comment']"/></span
-                  ><span class="user__profile-card-text">Savollarim</span>
+                    <font-awesome-icon :icon="['fas', 'comment']" />
+                  </span>
+                  <span class="user__profile-card-text">Savollarim</span>
                   <font-awesome-icon :icon="['fas', 'angle-right']" />
                 </template>
-                <b-card-text></b-card-text
-              ></b-tab>
+                <b-card-text></b-card-text>
+              </b-tab>
 
               <b-tab>
                 <template v-slot:title>
                   <span class="user__profile-card-icon">
-                    <font-awesome-icon :icon="['fas', 'phone']"/></span
-                  ><span class="user__profile-card-text"
-                    >Telefon konsultatsiya</span
-                  >
+                    <font-awesome-icon :icon="['fas', 'phone']" />
+                  </span>
+                  <span class="user__profile-card-text">Telefon konsultatsiya</span>
                   <font-awesome-icon :icon="['fas', 'angle-right']" />
                 </template>
-                <b-card-text
-                  ><a href="tel:+998946863999" class="user__profile-card-phone">
-                    Biz bilan hoziroq bog'lanishingiz mumkin.<br />Tel:
+                <b-card-text>
+                  <a href="tel:+998946863999" class="user__profile-card-phone">
+                    Biz bilan hoziroq bog'lanishingiz mumkin.
+                    <br />Tel:
                     <span>+998946863999</span>
                   </a>
-                </b-card-text></b-tab
-              >
+                </b-card-text>
+              </b-tab>
               <b-tab>
                 <template v-slot:title>
                   <span class="user__profile-card-icon">
-                    <font-awesome-icon :icon="['fas', 'file']"/></span
-                  ><span class="user__profile-card-text"
-                    >Hujjat buyurtmalarim</span
-                  >
+                    <font-awesome-icon :icon="['fas', 'file']" />
+                  </span>
+                  <span class="user__profile-card-text">Hujjat buyurtmalarim</span>
                   <font-awesome-icon :icon="['fas', 'angle-right']" />
                 </template>
-                <b-card-text>Tab contents 1</b-card-text></b-tab
-              >
+                <b-card-text>Tab contents 1</b-card-text>
+              </b-tab>
               <b-tab>
                 <template v-slot:title>
                   <span class="user__profile-card-icon">
-                    <font-awesome-icon :icon="['fas', 'bell']"/></span
-                  ><span class="user__profile-card-text">Bildirishnomalar</span>
+                    <font-awesome-icon :icon="['fas', 'bell']" />
+                  </span>
+                  <span class="user__profile-card-text">Bildirishnomalar</span>
                   <font-awesome-icon :icon="['fas', 'angle-right']" />
                 </template>
-                <b-card-text>Tab contents 1</b-card-text></b-tab
-              >
+                <b-card-text>Tab contents 1</b-card-text>
+              </b-tab>
               <b-tab>
                 <template v-slot:title>
                   <span class="user__profile-card-icon">
-                    <font-awesome-icon :icon="['fas', 'cogs']"/></span
-                  ><span class="user__profile-card-text">Sozlash</span>
+                    <font-awesome-icon :icon="['fas', 'cogs']" />
+                  </span>
+                  <span class="user__profile-card-text">Sozlash</span>
                   <font-awesome-icon :icon="['fas', 'angle-right']" />
                 </template>
                 <b-card-text>
                   <div class="user__profile-card-info">
-                    <b-col md="12">
-                      <p>Rasm</p>
-                      <label for="image">
-                        <img
-                          src="../assets/images/avatar.png"
-                          alt=""
-                          class="avatar__image"
-                        />
-                        <input
-                          type="file"
-                          placeholder="Faylni tanlash"
-                          id="image"
-                          class="avatar__input"
-                        />
-                      </label>
-                    </b-col>
-                    <b-col lg="12">
-                      <form action="" method="POST">
-                        <b-col lg="6">
-                          <label for="user__profile-name">Ism</label>
-                          <input type="text" id="user__profile-name">
-                        </b-col>
-                        <b-col lg="6">
-                          <label for="user__profile-surname">Familya</label>
-                          <input type="text" id="user__profile-surname">
-                        </b-col>
-                        <b-col lg="6">
-                          <label for="user__profile-lang">Foydalanish tili</label>
-                          <select name="" id="select__user-lang">
-                            <option value="1">O'zbek</option>
-                            <option value="2">Rus</option>
-                          </select>
-                        </b-col>
-                        <b-col lg="6">
-
-                        </b-col>
-                        <b-col lg="6">
-
-                        </b-col>
-                        <b-col lg="6">
-                          <label for="user__profile-email">Email</label>
-                        </b-col>
-                      </form>
-                    </b-col>
-                  </div> </b-card-text
-              ></b-tab>
+                    <b-row>
+                      <b-col md="12">
+                        <form action method="POST" class="user__profile-form">
+                          <b-row>
+                            <b-col md="12">
+                              <p>Rasm</p>
+                              <label for="image">
+                                <img src="../assets/images/avatar.png" alt class="avatar__image" />
+                                <input
+                                  type="file"
+                                  placeholder="Faylni tanlash"
+                                  id="image"
+                                  class="avatar__input"
+                                />
+                              </label>
+                            </b-col>
+                            <b-col lg="6">
+                              <label for="user__profile-name">Ism</label>
+                              <input type="text" id="user__profile-name" />
+                            </b-col>
+                            <b-col lg="6">
+                              <label for="user__profile-surname">Familya</label>
+                              <input type="text" id="user__profile-surname" />
+                            </b-col>
+                            <b-col lg="6">
+                              <label for="user__profile-lang">Foydalanish tili</label>
+                              <select name id="select__user-lang">
+                                <option value="1">O'zbek</option>
+                                <option value="2">Rus</option>
+                              </select>
+                            </b-col>
+                            <b-col lg="6">
+                              <label for="user__profile-email">Email</label>
+                              <input type="email" id="user__profile-email" />
+                            </b-col>
+                            <b-col lg="6">
+                              <label for="user__profile-city">Shahar</label>
+                              <select name id="user__profile-city">
+                                <option value="0" selected disabled>{{$t('region.noregion')}}</option>
+                                <option value="1">{{$t('region.toshkent')}}</option>
+                                <option value="2">{{$t('region.toshkentv')}}</option>
+                                <option value="3">{{$t('region.andijon')}}</option>
+                                <option value="4">{{$t('region.buxoro')}}</option>
+                                <option value="5">{{$t('region.jizzax')}}</option>
+                                <option value="6">{{$t('region.qoraqalpogistion')}}</option>
+                                <option value="7">{{$t('region.qashqadaryo')}}</option>
+                                <option value="8">{{$t('region.navoi')}}</option>
+                                <option value="9">{{$t('region.namangan')}}</option>
+                                <option value="10">{{$t('region.samarqand')}}</option>
+                                <option value="11">{{$t('region.surxondaryo')}}</option>
+                                <option value="12">{{$t('region.sirdaryo')}}</option>
+                                <option value="13">{{$t('region.fargona')}}</option>
+                                <option value="14">{{$t('region.xorazm')}}</option>
+                              </select>
+                            </b-col>
+                            <b-col lg="6">
+                              <label for="user__profile-number">Telefon raqam</label>
+                              <input type="text" id="user__profile-number">
+                            </b-col>
+                            <b-col lg="12">
+                              <b-button type="submit"><font-awesome-icon :icon="['fas','save']" />Saqlash</b-button>
+                              <b-button type="submit"><font-awesome-icon :icon="['fas','remove']" />Bekor qilish</b-button>
+                            </b-col>
+                          </b-row>
+                        </form>
+                      </b-col>
+                    </b-row>
+                  </div>
+                </b-card-text>
+              </b-tab>
             </b-tabs>
           </b-card>
         </div>
