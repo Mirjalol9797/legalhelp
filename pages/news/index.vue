2fddf97a84f3d06af863be77f92f49d188056725
Navruz <navruz.halimov.3999@gmail.com>
2020-10-14 23:04:36 +0500

    integration start

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,67 +1,101 @@
 <template>
   <div>
     <div class="news__wrapper">
-      <h1 class="news__heading">{{$t('news.title')}}</h1>
+      <h1 class="news__heading">{{ $t("news.title") }}</h1>
       <div class="news__header">
         <b-container>
           <b-row v-if="singlePost">
             <b-col lg="6" class="px-0">
-              <nuxt-link :to="localePath(`/news/${singlePost.id}`)" class="main__news-link">
-                <img src="../../assets/images/news/mainnews.jpg" height="380" width="702" alt="">
+              <nuxt-link
+                :to="localePath(`/news/${singlePost.id}`)"
+                class="main__news-link"
+              >
+                <img
+                  :src="$store.state.baseURL + singlePost.thubmnail"
+                  height="380"
+                  width="702"
+                  alt=""
+                />
               </nuxt-link>
             </b-col>
             <b-col lg="6">
-              <nuxt-link :to="localePath(`/news/${singlePost.id}`)" class="content-link">
+              <nuxt-link
+                :to="localePath(`/news/${singlePost.id}`)"
+                class="content-link"
+              >
                 <div class="news__content">
-                  <h6 class="news__title">{{ singlePost.title }} </h6>
-                  <div class="news__text">Lorem ipsum dolor sit amet consectetur adipiscing
-                    elit. Non nec, bibendum feugiat lectus eu sagittis adipiscing odio.Lorem ipsum dolor sit amet consectetur adipiscingelit. Non nec, bibendum feugiat lectus eu sagittis adipiscing odio. Lorem ipsum dolor sit amet consectetur adipiscing
-                    elit. Non nec, bibendum feugiat lectus eu sagittis adipiscing odio.Lorem ipsum dolor sit amet consectetur adipiscingelit. Non nec, bibendum feugiat lectus eu sagittis adipiscing odio.Lorem ipsum dolor sit amet consectetur adipiscing</div>
+                  <h6 class="news__title">{{ singlePost.title }}</h6>
+                  <div class="news__text" v-html="singlePost.intro">
+                  </div>
                   <div class="news__meta">
-                    <span class="news__meta-calendar">{{ singlePost.created_at }}</span>
-                    <span class="news__meta-view">{{ singlePost.view_count }}</span>
+                    <span class="news__meta-calendar">{{
+                      singlePost.created_at | moment("L")
+                    }}</span>
+                    <span class="news__meta-view">{{
+                      singlePost.view_count
+                    }}</span>
                   </div>
                 </div>
               </nuxt-link>
             </b-col>
           </b-row>
         </b-container>
-       
       </div>
       <div class="news__list">
-      	 <b-container>
-      	<b-row>
-      		<b-col lg="4" md="6" sm="6" v-for="item of news.slice(1,10)" :key="item.id" class="align-items-stretch">
-      			<div class="news__item">
-      				<nuxt-link :to="localePath(`/news/${item.id}`)" class="news__item-link">
-      				<img src="../../assets/images/news/news1.jpg" height="185" width="362" alt=""
-      				>
-      				<div class="news__item-content">
-      					<h6 class="news__item-content-title">{{ item.title }}</h6>
-      					<p class="news__item-content-text"> penatibus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sem massa lectus tortor feugiat sagittis auctor porta penatibus.</p>
-      					<div class="news__item-content-meta">
-      						<span class="news__item-content-meta-calendar">{{ item.created_at }}</span>
-      						<span class="news__item-content-meta-view">{{ item.view_count }}</span>
-      					</div>
-      					<div class="more__btn">
-      						<b-button type="button" class="question__btn">{{$t('news.more')}}</b-button>
-      					</div>
-      				</div>
-      			</nuxt-link>
-      			</div>
-      			
-      		</b-col>
-      	</b-row>
-      </b-container>
+        <b-container>
+          <b-row>
+            <b-col
+              lg="4"
+              md="6"
+              sm="6"
+              v-for="item of news.slice(1, 10)"
+              :key="item.id"
+              class="align-items-stretch"
+            >
+              <div class="news__item">
+                <nuxt-link
+                  :to="localePath(`/news/${item.id}`)"
+                  class="news__item-link"
+                >
+                  <img
+                    :src="$store.state.baseURL + item.thubmnail"
+                    height="185"
+                    width="362"
+                    alt=""
+                  />
+                  <div class="news__item-content">
+                    <h6 class="news__item-content-title">{{ item.title }}</h6>
+                    <p class="news__item-content-text" v-html="item.intro">
+                      
+                    </p>
+                    <div class="news__item-content-meta">
+                      <span class="news__item-content-meta-calendar">{{
+                        item.created_at
+                      }}</span>
+                      <span class="news__item-content-meta-view">{{
+                        item.view_count
+                      }}</span>
+                    </div>
+                    <div class="more__btn">
+                      <b-button type="button" class="question__btn">{{
+                        $t("news.more")
+                      }}</b-button>
+                    </div>
+                  </div>
+                </nuxt-link>
+              </div>
+            </b-col>
+          </b-row>
+        </b-container>
       </div>
-  
-        	<div class="pagination__menu">
+
+      <div class="pagination__menu">
         <b-pagination
           v-model="currentPage"
           :total-rows="rows"
           :per-page="perPage"
-          :first-text=" $t('pagination.first')"
-          :last-text=" $t('pagination.last')"
+          :first-text="$t('pagination.first')"
+          :last-text="$t('pagination.last')"
         ></b-pagination>
       </div>
     </div>
@@ -70,17 +104,17 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
 export default {
   data() {
     return {
       rows: 100,
       perPage: 1,
-      currentPage: 1,
+      currentPage: 1
     };
   },
-  computed :{
-    ...mapGetters(['news']),
+  computed: {
+    ...mapGetters(["news"]),
     singlePost() {
       return this.news[0];
     }
@@ -97,12 +131,9 @@ export default {
     // }
   },
   created() {
-    // this.loader = true;
-    this.$store.dispatch('getNews')
-    .then(() => {
-      // this.loader = false;
-      // console.log(this.news);
-    })
+    this.$store.dispatch("getNews").then(() => {
+      console.log(this.news);
+    });
   }
 };
 </script>
--- store/index.js
+++ store/index.js
@@ -4,29 +4,44 @@ Vue.use(Vuex);
 
 const store = () => new Vuex.Store({
     state: {
-        // data 
         news: [],
+        service:[],
+        baseURL: 'http://legaltechuz.herokuapp.com/api'
     },
     mutations: {
         setNews(state,payload) {
             state.news = payload;
-        }
+        },
+        setService(state,payload){
+            state.service = payload;
+        },
     },
     getters: {
         news(state) {
             return state.news;
+        },
+        service(state){
+            return state.service;
         }
     },
     actions: {
         async getNews({ commit }) {
             await this.$axios.get('posts/')
                 .then(res => {
+                    console.log(res.data.results);
                     commit('setNews',res.data.results);
                 })
                 .catch(err => {
                     console.log(err);
                 })
         },
+        async getService({commit}){
+            await this.$axios.get('')
+            .then(res =>{
+                console.log(res.data.results);
+                commit('setService',res.data.results);
+            })
+        }
         
     },
 })
