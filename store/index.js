0cbf9d88461c36efeb7c4f8ba596ebc733db456f
TolibRakhmonov <t.rakhmonov97@gmail.com>
2020-12-02 17:16:01 +0500

    Sign up finished

--- store/index.js
+++ store/index.js
@@ -10,8 +10,6 @@ const store = () => new Vuex.Store({
         user:{},
         baseURL: 'http://188.225.79.96/api',
         mediaURL: 'http://188.225.79.96/',
-        phone_number: "",
-        token: "",
         lawyers: []
     },
     getters: { 
@@ -29,12 +27,6 @@ const store = () => new Vuex.Store({
         }
     },
     mutations: {
-        setToken(state, token) {
-            state.token = token;
-        },
-        setPhone_number(state, phone_number) {
-            state.phone_number = phone_number;
-        },
         sendCode(state, payload) {
             state.code = payload
         },
@@ -49,24 +41,7 @@ const store = () => new Vuex.Store({
         }
         
     },
-    actions: {
-        async getCode({ commit }, data) {
-            await this.$axios.post('user/code/send/', { phone_number: data })
-                .then(res => {
-                    console.log("Code: ", res.data.code)
-                })
-                .catch(err => console.log(err))
-        },
-        async sendCode({ commit }, data) {
-            await this.$axios.post('user/code/check/', data)
-                .then(res => {
-                    commit('setToken', res.data.token)
-                    commit('setPhone_number', res.data.phone_number)
-                    console.log('SendCode', res)
-
-                })
-                .catch(err => console.log(err))
-        },      
+    actions: {     
         async getNews({ commit }) {
             await this.$axios.get('posts/')
                 .then(res => {
